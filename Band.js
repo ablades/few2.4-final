141df3a99646a8e03071eb0008da30fd65143680
ablades <audaris@gmail.com>
2020-12-09 13:42:14 -0600

    more styling

--- Band.js
+++ Band.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, SafeAreaView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
 import { bands } from './metal.js'
 
 function Item({ band }) {
 	return (
       <View style={styles.item}>
-          <Text style={styles.text}>{band.name} {band.origin} {band.fans} {band.formed}</Text>
+          <Text style={styles.title}>{band.name}</Text>
+          <Text style={styles.text}>Origin: {band.origin}{"\n"}Fan Count: {band.fans}{"\n"}Formed: {band.formed}</Text>
       </View>
 	)
 }
@@ -44,6 +45,11 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "white",
   },
   text: {
     color: "white",
